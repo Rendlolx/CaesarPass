fb8507ace7a965d06b29a3aaa2b9eb6ae8684ee5
Rendlolx <seriy_ragimov@mail.ru>
2022-02-15 20:01:15 +0300

    This is a new game for dummies!

---
+++ main.py
@@ -0,0 +1,25 @@
+print('Программа шифровки / дешифровки текста по методу Цезаря')
+k = int(input('Введите шаг сдвига для шифровки ( >0 ) или дешифровки ( <0 ): '))
+en_alphabet = [chr(i) for i in range(65, 91)] + [chr(j) for j in range(97, 123)]
+ru_alphabet = [chr(i) for i in range(1040, 1104)]
+
+
+def cezar(text):
+    if text[0] in en_alphabet:
+        alphabet = en_alphabet
+        shift = 26
+    else:
+        alphabet = ru_alphabet
+        shift = 32
+    for i in range(len(text)):
+        if text[i].isalpha():
+            if text[i].isupper():
+                print(alphabet[(alphabet.index(text[i]) + k) % shift], end='')
+            else:
+                print(alphabet[(alphabet.index(text[i]) + k) % shift + shift], end='')
+        else:
+            print(text[i], end='')
+
+
+txt = input('Введите текст: ')
+cezar(txt)
